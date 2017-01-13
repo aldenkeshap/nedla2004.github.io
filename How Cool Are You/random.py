1c8a2bedfe37f998f5adfdbdde67f6af1325ab9e
nedla2004 <alden@keshap.com>
2017-01-13 11:34:53 -0500

    How Cool Are You?

---
+++ How Cool Are You/random.py
@@ -0,0 +1,13 @@
+from browser import document
+
+def hash(name):
+    total = 0
+    for char in name:
+        total *= 10
+        total += ord(char)
+    return 10 - (total % 11)
+def update():
+    result = hash(document['name'].value)
+    document['result'].value = 'You are a %s/10' % result
+
+document['name'].bind('onSubmit', update)
